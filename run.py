c792d835f48bc95b78de4d36f10b7f15044be521
luffenoff <z.roma1337@gmail.com>
2024-10-27 21:51:01 +0500

    New version

---
+++ run.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "test")  # Используем значение по умолчанию, если переменная окружения не найдена
+
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "admin" and password == "admin123":
+            flash("Вход успешен", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Неверный логин или пароль", "error")
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Здесь можно добавить логику для сохранения пользователя
+        flash("Регистрация пройдена! Пожалуйста войдите.", "success")
+        return redirect(url_for('login'))  # Перенаправление на страницу логина
+    return render_template("register.html")
+
+@app.route("/forgot_password", methods=["GET", "POST"])
+def forgot_password():
+    if request.method == "POST":
+        email = request.form["email"]
+        flash("Сообщение для сброса пароля отправлено на вашу почту", "success")
+        return redirect(url_for('login'))  # Перенаправление на страницу логина
+    return render_template("forgot_password.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
