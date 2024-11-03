f966327410dac6d99531394505218dfa704fbea3
luffenoff <z.roma1337@gmail.com>
2024-11-04 01:08:25 +0500

    0.0.4.3

--- static/js/add_startup.js
+++ static/js/add_startup.js
@@ -3,18 +3,27 @@ function autoResize(textarea) {
     textarea.style.height = (textarea.scrollHeight) + 'px';
 }
 
-function CheckFrom() {
-    const name = document.getElementById('name').ariaValueMax.trim();
-
+function CheckForm() {
+    const name = document.getElementById('name').value.trim();
+    const description = document.getElementById('description').value.trim();
+    const image = document.getElementById('image').value;
+    const submitBtn = document.getElementById('submitBtn');
+    submitBtn.disabled = !(name && description && image);
 }
 
+function previewImage() {
+    const imageInput = document.getElementById('image');
+    const preview = document.getElementById('imagePreview');
 
-
-
-
-
-
-
-document.getElementById("image").addEventListener("change", function (event) {
-
+    const file = imageInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = "block";
+        };
+        reader.readAsDataURL(file);
+    } else {
+        preview.style.display = "none";
+    }
 }
